9361a03a2af90f474ecb4a52651ca1885085fe4e
agustinmsanjuan <asanjun@frba.utn.edu.ar>
2023-07-19 11:39:27 -0300

    medios de notif heredan metodo adaptarMensaje

--- Entrega-3/src/main/java/dominio/Notificacion/MedioCorreo.java
+++ Entrega-3/src/main/java/dominio/Notificacion/MedioCorreo.java
@@ -7,21 +7,10 @@ import java.util.List;
 
 public class MedioCorreo extends MedioDeComunicacion {
     CorreoAPI correoAPI;
-    String mensajes;
 
     @Override
     public void notificarIncidentes(Miembro miembro, List<Incidente> incidentes, Comunidad comunidad) {
-        // Formatea el mensaje (los adapta a la API de correo como un string)
-        // para cada incidente en incidentes, se concatena el mensaje con un "\n"
-        // TODO: implementar para agrupaciones
-        incidentes.forEach(incidente -> {
-            String servicio = incidente.getServicio().getNombre();
-            String observacion = incidente.getObservacion();
-            String nombreComunidad = comunidad.getNombre();
-            mensajes = "Se ha reportado un incidente en la comunidad " + nombreComunidad + ".\n" + "Servicio: " + servicio + "\n" + observacion;
-            mensajes += "\n";
-        });
-
+        adaptarMensaje(incidentes, comunidad);
         correoAPI.enviarCorreo(miembro.getCorreo(), mensajes);
     }
 }
--- Entrega-3/src/main/java/dominio/Notificacion/MedioDeComunicacion.java
+++ Entrega-3/src/main/java/dominio/Notificacion/MedioDeComunicacion.java
@@ -7,6 +7,19 @@ import dominio.comunidades.Miembro;
 import java.util.List;
 
 public abstract class MedioDeComunicacion {
+     String mensajes;
      public abstract void notificarIncidentes(Miembro miembro, List<Incidente> incidentes, Comunidad comunidad);
+     public void adaptarMensaje(List<Incidente> incidentes, Comunidad comunidad) {
+          // Formatea el mensaje (los adapta a la API de correo como un string)
+          // para cada incidente en incidentes, se concatena el mensaje con un "\n"
+          // TODO: implementar para agrupaciones
+          incidentes.forEach(incidente -> {
+               String servicio = incidente.getServicio().getNombre();
+               String observacion = incidente.getObservacion();
+               String nombreComunidad = comunidad.getNombre();
+               mensajes = "Se ha reportado un incidente en la comunidad " + nombreComunidad + ".\n" + "Servicio: " + servicio + "\n" + observacion;
+               mensajes += "\n";
+          });
+     }
 
 }
