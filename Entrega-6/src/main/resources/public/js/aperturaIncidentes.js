55275a32600d40b2c4d73e49b6fb33ad23a6ad02
alejosandrini <alejo.sandrini@gmail.com>
2023-11-12 12:17:31 -0300

    Arreglo carga de options asincrona en chosen js

--- Entrega-6/src/main/resources/public/js/aperturaIncidentes.js
+++ Entrega-6/src/main/resources/public/js/aperturaIncidentes.js
@@ -21,18 +21,51 @@ function abrirIncidente(){
     })
 }
 
+Vue.component('select-servicios', {
+    // props:{
+    //     servicios: {
+    //         type: Array,
+    //         required: true
+    //     }
+    // },
+    template:`
+        <select class="chosen-select" multiple>
+            <option v-for="servicio in servicios"
+                    :key="servicio.id" 
+                    :value="servicio.id">
+                {{servicio.nombre}}
+            </option>
+        </select>`,
+
+    data(){
+        return {
+            servicios: []
+            //servicios:[{id:200,nombre:"servicio 2"}, {id:400,nombre:"servicio baño coto"}]
+        }
+    },
+    created() {
+        this.fetchData();
+    },
+    methods: {
+        async fetchData() {
+            try {
+                const response = await fetch('http://localhost:4567/api/servicios');
+                const data = await response.json();
+                this.servicios = data; // Asignar los datos a tu propiedad de datos
+                this.$nextTick(() => {
+                    // Llamar a la función de inicialización del plugin Chosen aquí
+                    $(this.$el).trigger('chosen:updated');
+                });
+            } catch (error) {
+                console.error('Error al obtener datos:', error);
+            }
+        },
+    }
+})
+
 var app = new Vue({
     el: '#app-select',
-    data:{
-        servicios:[]
-    },
-    created () {
-
-        fetch('http://localhost:4567/api/servicios')
-            .then(response => response.json())
-            .then(data => {
-                this.servicios = data
-            })
-    }
-
+    // data:{
+    //     servicios:[]
+    // },
 })
